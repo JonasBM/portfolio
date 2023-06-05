2b34f6fdd2dfe7cf348405bed046ba67a3b3daff
Jonas Burigo Martins <jonas@jonasbm.com.br>
2023-06-05 13:07:59 -0300

    feature: arrow down button function

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -25,6 +25,10 @@ const Main = () => {
     };
   }, [handleShadow]);
 
+  const scrollIntoAbout = () => {
+    document.getElementById("about")?.scrollIntoView();
+  };
+
   return (
     <div id="home" className="w-full h-screen text-center">
       <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
@@ -48,10 +52,11 @@ const Main = () => {
         </div>
       </div>
       {showArrow && (
-        <div className="flex justify-center absolute bottom-20 w-full ">
+        <div className="flex justify-center absolute bottom-10 w-full ">
           <div
-            className="rounded-full animate-pulse shadow-lg shadow-gray-400 p-4 hover:scale-110 ease-in duration-300"
+            className="rounded-full animate-pulse shadow-lg shadow-gray-400 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
             title="See more"
+            onClick={scrollIntoAbout}
           >
             <HiOutlineChevronDoubleDown className="text-[#5651e5]" size={30} />
           </div>
